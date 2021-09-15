7347bc526b4e67a9a1d1717d46e4b88330baa4b8
Rhandom1 <73659834+Rhandom1@users.noreply.github.com>
2021-09-15 12:16:59 -0500

    Separated sql js and set up inquirer prompts

--- server.js
+++ server.js
@@ -1,23 +1,77 @@
-const express = require('express');
-// Import and require mysql2
-const mysql = require('mysql2');
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-const PORT = process.env.PORT || 3001;
-const app = express();
+const trackArray = [];
+const deptArray = [];
+const roleArray = [];
+const employeeArray = [];
 
-// Express middleware
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+const trackEmp = [
+    {
+        type: "list",
+        message: "What would you like to do?",
+        name: "table",
+        choices: ["Show All Employees", "Show All Roles", "Show Leads", "Add New Department", "Add New Role", "Add New Employee", "Update Employee"]
+    }
+];
 
-// Connect to database
-const db = mysql.createConnection(
-  {
-    host: 'localhost',
-    // MySQL username,
-    user: 'root',
-    // MySQL password
-    password: '',
-    database: ''
-  },
-  console.log(`Connected to the database.`)
-);
+//Add Department
+const deptQuestions = [
+    {
+        type: "input",
+        message: "What Department would you like to add?"
+        name: "name"
+    }
+];
+
+//Add role
+const roleQuestions = [
+    {
+        type: "input",
+        message: "What role would you like to add?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Please, enter a salary for this role:",
+        name: "salary"
+    }
+];
+
+//Add Employee
+const employeeQuestions = [
+    {
+        type: "input",
+        message: "Please, enter the new employee's first name:",
+        name: "first name"
+    },
+    {
+        type: "input",
+        message: "Please, enter the new employee's last name:",
+        name: "last name"
+    },
+    {
+        type: "list",
+        message: "Please, select the new employee's role:",
+        name: "role"
+        choices: ["Sales", "Front-End Developer", "Back-End Developer", "Accounant", "Legal"]
+    },
+    {
+        type: "input",
+        message: "Please, enter the new employee's salary:",
+        name: "salary"
+    }
+];
+
+
+
+//Add Department
+    //Add name to db
+
+    //Add name, salary, and dept to role db
+//Add Employee
+    //Add first name, last name, role, and manager to employee db
+//Update Employee
+    //Show list of employees
+        //Alter role or salary and update in db
+//Delete employee, role, or Department (BONUS)
